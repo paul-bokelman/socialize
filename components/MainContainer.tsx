196241183f6582c4d0e5cbaa0e4b6b873e64f5b5
Paul Bokelman <paul.bokelman1@gmail.com>
2021-11-08 14:52:16 -0800

    removed footer, adjusted vw

--- components/MainContainer.tsx
+++ components/MainContainer.tsx
@@ -7,14 +7,14 @@ export const MainContainer = ({
   children: React.ReactChild[];
 }): JSX.Element => {
   return (
-    <div>
+    <div className="w-[100vw]">
       <Seo />
-      <div className="flex flex-col w-[100vw] mb-12 justify-center items-center">
+      <div className="flex flex-col w-full mb-12 justify-center items-center">
         <div className="w-[90vw] md:w-5/6 mt-24 border-2 dark:border-[#30363C] rounded-t-lg p-2">
           <Header />
         </div>
         <div className="w-[90vw] md:w-5/6 -mt-0.5 border-2 dark:border-[#30363C] rounded-b-lg">
-          <div className="prose prose-md dark:prose-dark m-8 !max-w-full">
+          <div className="prose md:prose-md prose-xs dark:prose-dark m-8 !max-w-full">
             {children}
           </div>
         </div>
